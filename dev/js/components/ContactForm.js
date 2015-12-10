29c9418ac3a9e0114a11f901d410a62bfe8f1ca9
Dan Steinberg <daniel_steinberg@intuit.com>
2015-12-10 13:48:15 -0800

    Fixed require and other merge issues

--- dev/js/components/ContactForm.js
+++ dev/js/components/ContactForm.js
@@ -3,10 +3,12 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 
 var FormLabel = require("./FormLabel.js");
-var SubmitButton = require("./SubmitButton");
-var PhoneNumber = require("./PhoneNumber")
-
+var ContactName = require("./nameInput.js");
+var ContactEmail = require("./ContactEmail");
+var PhoneNumber = require("./PhoneNumber");
 var ContactZip = require("./ContactZip.js");
+var SubmitButton = require("./SubmitButton");
+
 var ContactForm = React.createClass({
 
 	render: function() {
