1f566298fe928bbae228ec9da92a6f238e6bca55
Craig McCarley <craig_mccarley@intuit.com>
2015-12-10 10:53:42 -0800

    Initial addition of Phone Number component

---
+++ dev/js/components/phonenumber.js
@@ -0,0 +1,15 @@
+/** @jsx React.DOM */
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var PhoneNumber = React.createClass({
+    render:function(){
+        return (
+            <div className="wrapper">
+                <input type="number" placeholder="Phone Number" name="phoneNumber" />
+            </div>
+        )
+    }
+});
+
+module.exports = PhoneNumber;
