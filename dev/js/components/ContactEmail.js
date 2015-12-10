04806b2f62c740dd7a526de8bc1d7d80afff3b7c
sramya <saurabh_ramya@intuit.com>
2015-12-10 11:49:21 -0800

    Add placeholder

--- dev/js/components/ContactEmail.js
+++ dev/js/components/ContactEmail.js
@@ -5,7 +5,7 @@ var ContactEmail = React.createClass ({
 	render: function () {
 		return (
 			<div>
-        		Email: <input type="email" name="email" /> <br />
+        		Email: <input type="email" placeholder="email" /> <br />
       		</div>
 		)
 	}
